d17a74a95ab33bbf961f7a744f9028134d97c8fa
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2022-02-03 18:04:26 +0100

    Set up search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import React from "react";
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hell, this is Andrea.
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <p>Hello, this is my dictionary.</p>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Andrea R.</footer>
+      </div>
     </div>
   );
 }
