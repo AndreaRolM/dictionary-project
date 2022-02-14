152d5d71c3f09b6cc2a309aab5ff51018e8995ff
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2022-02-14 13:31:14 +0100

    Rendered synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,9 +9,11 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            {definition.definition}
+            <strong>Definition:</strong> {definition.definition}
             <br />
-            <em>{definition.example}</em>
+            <em>Example: {definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
